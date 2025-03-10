52969d7a848969ee64e3b5648aad56906d2dca74
shawn <173310929+shawntarou@users.noreply.github.com>
2025-03-10 06:10:04 -0700

    Add sorted array to BST recursion

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,6 +1,28 @@
 class Tree
-  def initialize()
-    
+  def initialize(array)
+    @root = build_tree(array)
+  end
+  
+  def build_tree(array)
+    array.sort!
+    sorted_array_to_bst(array)
+  end
+
+  def sorted_array_to_bst(array)
+    sorted_array_to_bst_recursion(array, 0, array.length-1)
+  end
+
+  def sorted_array_to_bst_recursion(array, start, finish)
+    return nil if start > finish
+
+    mid = start + (finish - start) / 2
+
+    root = Node(array[mid])
+
+    root.left = sorted_array_to_bst_recursion(array, start, mid - 1)
+    root.right = sorted_array_to_bst_recursion(array, mid + 1, finish)
+
+    root
   end
 
   class Node
