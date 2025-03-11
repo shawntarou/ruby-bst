a45b846731cfe48923a84075a5a862e521dd568a
shawn <173310929+shawntarou@users.noreply.github.com>
2025-03-10 23:13:27 -0700

    Add insert/delete BST functions

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,10 +2,26 @@ class Tree
   def initialize(array)
     @root = build_tree(array)
   end
+
+  def insert(value)
+    insert_recursion(@root, value)
+  end
   
+  def delete(value)
+    delete_recursion(@root, value)
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  private 
+
   def build_tree(array)
-    array.sort!
-    sorted_array_to_bst(array)
+    sorted_array = array.sort.uniq
+    sorted_array_to_bst(sorted_array)
   end
 
   def sorted_array_to_bst(array)
@@ -16,7 +32,6 @@ class Tree
     return nil if start > finish
 
     mid = start + (finish - start) / 2
-
     root = Node.new(array[mid])
 
     root.left = sorted_array_to_bst_recursion(array, start, mid - 1)
@@ -25,12 +40,41 @@ class Tree
     root
   end
 
-  def insert(value)
+  def insert_recursion(root, value)
+    return Node.new(value) if root.nil?
+    return root if root.data == value
 
+    if root.data < value then root.right = insert_recursion(root.right, value) 
+    else root.left = insert_recursion(root.left, value) end
+
+    root
   end
 
-  def delete(value)
-    
+  def delete_recursion(root, value)
+    return root if root.nil?
+
+    if root.data > value then root.left = delete_recursion(root.left, value) 
+    elsif root.data < value then root.right = delete_recursion(root.right, value) 
+
+    else
+      return root.right if root.left.nil?
+      return root.left if root.right.nil?
+
+      succ = get_successsor(root)
+      root.data = succ.data
+      root.right = delete_recursion(root.right, succ.data)
+    end
+
+    root  
+  end
+
+  def get_successsor(curr)
+    curr = curr.right
+    while !curr.nil? && !curr.left.nil? 
+      curr = curr.left
+    end
+
+    curr
   end
 
   def find(value)
@@ -67,12 +111,6 @@ class Tree
 
   def rebalance
 
-  end
-
-  def pretty_print(node = @root, prefix = '..', is_left = true)
-    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
   class Node
--- main.rb
+++ main.rb
@@ -4,4 +4,9 @@ require_relative 'lib/tree.rb'
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 BST = Tree.new(array)
 
+BST.insert(6)
+BST.pretty_print
+BST.delete(8)
+BST.pretty_print
+BST.delete(9)
 BST.pretty_print
